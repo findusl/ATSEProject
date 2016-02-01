22b163c81eec32dd820b480f7d861e35345cd4f2
Stefan Fochler <istefo@me.com>
2016-02-01 10:51:43 +0100

    Bring back BlobEvelope and BlobEntry for syncing

--- Web/src/main/java/de/tum/score/transport4you/web/UserResource.java
+++ Web/src/main/java/de/tum/score/transport4you/web/UserResource.java
@@ -6,11 +6,14 @@ import org.restlet.resource.ServerResource;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.DatatypeConverter;
 
 import com.googlecode.objectify.ObjectifyService;
 
+import de.tum.score.transport4you.shared.mobilebusweb.data.impl.BlobEntry;
+import de.tum.score.transport4you.shared.mobilebusweb.data.impl.BlobEnvelope;
 import de.tum.score.transport4you.shared.mobilebusweb.data.impl.DataManager;
 import de.tum.score.transport4you.shared.mobilebusweb.data.impl.ETicket;
 
@@ -36,24 +39,32 @@ public class UserResource extends ServerResource {
 		// find tickets for the user
 		List<ETicket> tickets = DataManager.getInstance().getETicketsForUser(u.id);
 		
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ObjectOutputStream oos = new ObjectOutputStream(baos);
-		
 		// create the encrypted version for each ticket and save it to output
 		for (ETicket ticket : tickets) {
 			ticket.encryptTicket();
-			oos.writeObject(ticket);
 		}
 		
-		String ticketsBase64 = DatatypeConverter.printBase64Binary(baos.toByteArray());
-
+		// create a blob entry from tickets
+		BlobEntry entry = new BlobEntry();
+		entry.seteTicketList(new ArrayList<ETicket>(tickets));
+		entry.setUserId(u.id.toString());
+		entry.setUserName(u.name);
+		entry.setUserAddress(u.email);
+		
+		// create and serialize a blob envelope to base64
+		BlobEnvelope envelope = new BlobEnvelope(entry, null);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ObjectOutputStream oos = new ObjectOutputStream( baos );
+		oos.writeObject(envelope);
+		oos.close();
+		String base64envelope = DatatypeConverter.printBase64Binary(baos.toByteArray()); 
 		
 		String result = "";	
 		result += "<user>\n";
 		result += "\t<id>"		+ u.id 		+ "</id>\n";
 		result += "\t<name>"	+ u.name 	+ "</name>\n";
 		result += "\t<email>"	+ u.email 	+ "</email>\n";
-		result += "\t<ticket>"	+ ticketsBase64	+ "</ticket>\n";
+		result += "\t<ticket>"	+ base64envelope + "</ticket>\n";
 		result += "</user>\n";
         return result;
 		
