91a425eed4601f014757a04e1714f7f15bec49fc
Stefan Fochler <istefo@me.com>
2016-02-02 09:33:42 +0100

    formatting fixes

--- Web/src/main/java/de/tum/score/transport4you/web/UserResource.java
+++ Web/src/main/java/de/tum/score/transport4you/web/UserResource.java
@@ -19,55 +19,53 @@ import de.tum.score.transport4you.shared.mobilebusweb.data.impl.ETicket;
 
 import com.googlecode.objectify.Key;
 
-
-
 public class UserResource extends ServerResource {
 
-    @Get
-    public String represent() throws Exception {
-		User u = ObjectifyService.ofy().load().type(User.class).filter("email",getAttribute("email")).first().now();
+	@Get
+	public String represent() throws Exception {
+		User u = ObjectifyService.ofy().load().type(User.class).filter("email", getAttribute("email")).first().now();
 
-		if(!getAttribute("password").equals(u.password) ){
+		if (!getAttribute("password").equals(u.password)) {
 			return "<error>Wrong Password!</error>";
 		}
 
-		if(u == null){
-			System.err.println("User with email '"+getAttribute("email")+"' is null!");
+		if (u == null) {
+			System.err.println("User with email '" + getAttribute("email") + "' is null!");
 			return "";
 		}
-		
+
 		// find tickets for the user
 		List<ETicket> tickets = DataManager.getInstance().getETicketsForUser(u.id);
-		
+
 		// create the encrypted version for each ticket and save it to output
 		for (ETicket ticket : tickets) {
 			ticket.encryptTicket();
 		}
-		
+
 		// create a blob entry from tickets
 		BlobEntry entry = new BlobEntry();
 		entry.seteTicketList(new ArrayList<ETicket>(tickets));
 		entry.setUserId(u.id.toString());
 		entry.setUserName(u.name);
 		entry.setUserAddress(u.email);
-		
+
 		// create and serialize a blob envelope to base64
 		BlobEnvelope envelope = new BlobEnvelope(entry, null);
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ObjectOutputStream oos = new ObjectOutputStream( baos );
+		ObjectOutputStream oos = new ObjectOutputStream(baos);
 		oos.writeObject(envelope);
 		oos.close();
-		String base64envelope = DatatypeConverter.printBase64Binary(baos.toByteArray()); 
-		
-		String result = "";	
+		String base64envelope = DatatypeConverter.printBase64Binary(baos.toByteArray());
+
+		String result = "";
 		result += "<user>\n";
-		result += "\t<id>"		+ u.id 		+ "</id>\n";
-		result += "\t<name>"	+ u.name 	+ "</name>\n";
-		result += "\t<email>"	+ u.email 	+ "</email>\n";
-		result += "\t<ticket>"	+ base64envelope + "</ticket>\n";
+		result += "\t<id>" + u.id + "</id>\n";
+		result += "\t<name>" + u.name + "</name>\n";
+		result += "\t<email>" + u.email + "</email>\n";
+		result += "\t<ticket>" + base64envelope + "</ticket>\n";
 		result += "</user>\n";
-        return result;
-		
-    }
+		return result;
+
+	}
 
 }
